b6d9be39c166794313d18833a32debd8bd6dc941
Marion Poljakov <marion.poljakov@gmail.com>
2024-11-14 10:00:28 +0200

    Skaffolding usage, query fix

--- query/index.js
+++ query/index.js
@@ -1,45 +1,62 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { randomBytes } = require('crypto')   
 const cors = require('cors')
 const axios = require('axios')
 
-const app = express()
 
+const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-const posts = {}  
+const posts = {}
+
+const handleEvent = (type, data) => {
+    if (type === 'PostCreated') {
+        const { id, title } = data
+        
+        posts[id] = { id, title, comments: [] }  
+    } 
+
+    if (type === 'CommentCreated') {
+        const { id, content, postId, status } = data
+        
+        const post = posts[postId]
+        post.comments.push({ id, content, status })   
+    }
+
+    if (type ===  "CommentUpdated") {
+        const { id, content, postId, status } = data
+
+        const post = posts[postId]
+        const comment = post.comments.find(comment => {
+            return comment.id === id
+        })
+
+        comment.status = status
+        comment.content = content
+    } 
+} 
 
 app.get('/posts', (req, res) => {
     res.send(posts)
 })
-app.post('/posts/create', async (req, res) => {
-    const id  = randomBytes(4).toString('hex')
-    const { title } = req.body 
 
-    posts[id] = {
-        id,
-        title
-    } 
-    
-await axios.post('http://event-bus-srv:4005/events', {
-    type: 'PostCreated',
-    data: {
-        id, title
-    } 
-})
+app.post('/events', (req, res) => {
+    const { type, data } = req.body
 
-    res.status(201).send(posts[id])
-})
-
-app.post('/events',  (req, res) => {
-    console.log('Received Event', req.body.type)
+    handleEvent(type, data)
 
     res.send({})
 })
 
-app.listen(4000, () => {
-    console.log('v55')
-    console.log('Listening on 4000')
+app.listen(4002, async () => {
+    console.log('Listening on 4002')
+
+    const res = await axios.get('http://event-bus-srv:4005/events')
+
+    for (let event of res.data) {
+        console.log('Processing event:', event.type)
+
+        handleEvent(event.type, event.data)
+    } 
 })
